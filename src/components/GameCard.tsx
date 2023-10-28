cc7c7225559d425645ca393a05e77c030ab1558a
Muhammad Adnan <61709395+Adnan-Dani@users.noreply.github.com>
2023-10-28 20:38:40 +0500

    get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,6 +7,7 @@ import { Game } from '../hooks/useGames'
 // Custom Components
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
+import getCroppedImageUrl from '../services/image-url'
 
 interface Props {
     game: Game
@@ -15,7 +16,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
     return (
         <Card borderRadius={10} overflow='hidden'>
-            <Image src={game.background_image} />
+            <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize="2xl">{game.name}</Heading>
                 <HStack justifyContent="space-between">
