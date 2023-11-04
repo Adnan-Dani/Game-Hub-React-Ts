11640a04583f4c15d032d17b8533c5ad9063407e
Muhammad Adnan <61709395+Adnan-Dani@users.noreply.github.com>
2023-11-04 11:48:28 +0500

    cleanup the game card

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -20,7 +20,7 @@ const GamesGrid = ({ gameQuery }: Props) => {
     return (
         <div>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={3} padding='10'>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6} padding='10'>
                 {isLoading && skeltons.map(s => <GameCardContainer key={s}><GameCardSkelton /></GameCardContainer>)}
                 {
                     data.map(g => <GameCardContainer key={g.id}><GameCard game={g} /></GameCardContainer>)
